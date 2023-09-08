9e419a4239bff363a7b3639fa43e9f498bcbec75
J. G. M <87867987+VirtueDevOps@users.noreply.github.com>
2023-09-08 12:56:05 -0400

    Update ML_Brewing_PM.py

--- ML_Brewing_PM.py
+++ ML_Brewing_PM.py
@@ -1,33 +1,48 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Step 1: Collect and preprocess data
 # Replace this code with your own data preprocessing steps
 data = pd.read_csv("brewing_data.csv")
 X = data.drop("failure", axis=1)
 y = data["failure"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Step 2: Explore and visualize data
 # Replace this code with your own data exploration and visualization steps
 print(X_train.describe())
 
+# Visualize the distribution of target variable
+plt.figure()
+y.value_counts().plot(kind='bar')
+plt.title('Distribution of Failure')
+plt.xlabel('Failure')
+plt.ylabel('Count')
+plt.show()
+
 # Step 3: Select a machine learning algorithm
 # Replace this code with your own algorithm selection steps
-model = RandomForestClassifier()
+model = RandomForestClassifier(random_state=42)
 
 # Step 4: Train and evaluate the model
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+print(f"Classification Report:\n{classification_report(y_test, y_pred)}")
+print(f"Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}")
 
 # Step 5: Deploy the model
 # Replace this code with your own model deployment steps
 def predict_failure(data):
-  return model.predict(data)
+  try:
+      return model.predict(data)
+  except Exception as e:
+      return str(e)
 
 # Step 6: Monitor and maintain the model
 # Replace this code with your own model monitoring and maintenance steps
+
